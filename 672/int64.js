19b0a7f072846a9610feb81a206815e2bdda0e4e
ishak <ishakfg14@gmail.com>
2026-04-16 21:01:20 +0200

    Update int64.js

--- 672/int64.js
+++ 672/int64.js
@@ -1,25 +1,38 @@
-function int64(low, hi) {
-    this.low = low;
-    this.hi = hi;
+// ملف int64.js الأساسي لثغرات PS4
+function Int64(low, high) {
+    var res = new Uint8Array(8);
+    res[0] = low & 0xff;
+    res[1] = (low >> 8) & 0xff;
+    res[2] = (low >> 16) & 0xff;
+    res[3] = (low >> 24) & 0xff;
+    res[4] = high & 0xff;
+    res[5] = (high >> 8) & 0xff;
+    res[6] = (high >> 16) & 0xff;
+    res[7] = (high >> 24) & 0xff;
+    this.buffer = res;
 }
 
-int64.prototype.add32 = function(val) {
-    var new_low = (this.low + val) >>> 0;
-    if (new_low < this.low) this.hi++;
-    this.low = new_low;
-}
+Int64.prototype.low = function() {
+    return (this.buffer[3] << 24) | (this.buffer[2] << 16) | (this.buffer[1] << 8) | this.buffer[0];
+};
 
-int64.prototype.set32 = function(val) {
-    this.low = val;
-    this.hi = 0;
-}
+Int64.prototype.high = function() {
+    return (this.buffer[7] << 24) | (this.buffer[6] << 16) | (this.buffer[5] << 8) | this.buffer[0];
+};
 
-function int64_from_float(f) {
-    var bytes = new Uint8Array(8);
-    var farr = new Float64Array(bytes.buffer);
-    farr[0] = f;
-    return new int64(
-        bytes[0] | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24),
-        bytes[4] | (bytes[5] << 8) | (bytes[6] << 16) | (bytes[7] << 24)
-    );
+Int64.prototype.toString = function() {
+    var res = "";
+    for (var i = 7; i >= 0; i--) {
+        var h = this.buffer[i].toString(16);
+        if (h.length < 2) res += "0";
+        res += h;
+    }
+    return "0x" + res;
+};
+
+// وظائف إضافية للتعامل مع العناوين في الذاكرة
+function Add(a, b) {
+    var l = (a.low() + b.low()) >>> 0;
+    var h = (a.high() + b.high() + (l < a.low() ? 1 : 0)) >>> 0;
+    return new Int64(l, h);
 }
