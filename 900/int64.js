ab5a107a5f86d794aabc44adecfdb8996c55942c
ishak <ishakfg14@gmail.com>
2026-04-17 16:27:57 +0200

    Update int64.js

--- 900/int64.js
+++ 900/int64.js
@@ -1,13 +1,11 @@
-// int64.js - التعامل مع ذاكرة 64-بت
 var Int64 = function(low, high) {
-    var float64 = new Float64Array(1);
-    var uint32 = new Uint32Array(float64.buffer);
-    this.low = low; this.high = high;
-    this.add = function(other) {
-        var l = (this.low + other.low) >>> 0;
-        var h = (this.high + other.high + (l < this.low ? 1 : 0)) >>> 0;
-        return new Int64(l, h);
-    };
+    this.low = low;
+    this.high = high;
 };
-// دوال إضافية للتحويل من وإلى التنسيقات المختلفة
-function int64(low, high) { return new Int64(low, high); }
+
+Int64.prototype.add = function(other) {
+    var low = (this.low + other.low) >>> 0;
+    var high = (this.high + other.high + (low < this.low ? 1 : 0)) >>> 0;
+    return new Int64(low, high);
+};
+// ... (بقية العمليات الحسابية)
